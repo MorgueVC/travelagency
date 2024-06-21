b8644101e34173c1b41123b31af2b7e07f753c9d
MorgueVC <153827722+MorgueVC@users.noreply.github.com>
2024-06-21 09:32:55 +0200

    Add files via upload
    code om trips te deleten van account pagina

---
+++ deletetrip_query.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit();
+}
+
+require_once 'conn.php';
+
+$connection = new PDO('mysql:dbname=travelweb;host=travel_db', 'root', 'rootpassword');
+
+
+$username = $_SESSION['username'];
+
+$query = "DELETE FROM bookings WHERE username = :username";
+$stmt = $connection->prepare($query);
+$stmt->bindParam(':username', $username);
+$result = $stmt->execute();
+
+if (!$result) {
+    die("Query Failed: " . $stmt->errorInfo());
+} else {
+    header("Location: accountmanagement.php");
+    exit;
+}
+?>
